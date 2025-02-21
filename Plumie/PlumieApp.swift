86d036cda9920c831bd10a02c97c930d89d072b4
msuhrooom <danbinluo@outlook.com>
2025-02-21 18:30:50 +0000

    Initial Commit

---
+++ Plumie/PlumieApp.swift
@@ -0,0 +1,20 @@
+//
+//  PlumieApp.swift
+//  Plumie
+//
+//  Created by Danbin Luo on 21/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlumieApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ PlumieTests/PlumieTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlumieTests.swift
+//  PlumieTests
+//
+//  Created by Danbin Luo on 21/02/2025.
+//
+
+import Testing
+@testable import Plumie
+
+struct PlumieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
